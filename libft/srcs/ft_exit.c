a6ca064d98ee72354c65cb0f1370b8b025a19658
Gcol <colletto.greogry@gmail.com>
2017-12-13 11:58:57 +0100

    retry lib

--- libft/srcs/ft_exit.c
+++
@@ -1,8 +0,0 @@
-#include "libft.h"
-
-void  ft_exit(long motif)
-{
-  if (motif == 2 || motif < 2 || motif > 2)
-    write(2, "Error\n", 6);
-  exit(motif);
-}
--- libft/srcs/ft_isspace.c
+++
@@ -1,7 +0,0 @@
-
-int		ft_isspace(char c)
-{
-	if ((c >= 8 && c <= 13) || c == 32)
-		return (1);
-	return (0);
-}
